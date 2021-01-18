91e0f88280c6d24caee58c4f3d696b9f1b45a844
sarahbraka <69284826+sarahbraka@users.noreply.github.com>
2021-01-18 17:45:04 -0500

    Update beyond_campus_articles.js

--- src/data/beyond_campus_articles.js
+++ src/data/beyond_campus_articles.js
@@ -1,38 +1,44 @@
 export const beyond_campus_articles = [
     {
-        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/37EFT3YKOJE2ZCAARGOM6TQEGQ.png",
-        article_title: "How to choose and manage extracurriculars as a first-year",
-        article_authors: "Victoria Yang",
-        article_link: "https://www.columbiaspectator.com/required-reading/2017/07/24/how-to-choose-and-manage-extracurriculars-as-a-first-year/"
+        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/WIUXZQFBPVA3JNOTH46T4SA22U.jpg",
+        article_title: "As classrooms confine students to discussing social issues in theory, many have returned home to enact real change",
+        article_authors: "Dia Gill",
+        article_link: "https://www.columbiaspectator.com/news/2020/09/23/as-classrooms-confine-students-to-discussing-social-issues-in-theory-many-have-returned-home-to-enact-real-change/"
     },
     {
-        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/37EFT3YKOJE2ZCAARGOM6TQEGQ.png",
-        article_title: "How to choose and manage extracurriculars as a first-year",
-        article_authors: "Victoria Yang",
-        article_link: "https://www.columbiaspectator.com/required-reading/2017/07/24/how-to-choose-and-manage-extracurriculars-as-a-first-year/"
+        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/JS2KNHLY7ZAY5LQADKLATGVGFM.jpg",
+        article_title: "How could higher education change under the Biden administration?",
+        article_authors: "Kathy Fang",
+        article_link: "https://www.columbiaspectator.com/news/2020/12/04/how-could-higher-education-change-under-the-biden-administration/"
     },
     {
-        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/37EFT3YKOJE2ZCAARGOM6TQEGQ.png",
-        article_title: "How to choose and manage extracurriculars as a first-year",
-        article_authors: "Victoria Yang",
-        article_link: "https://www.columbiaspectator.com/required-reading/2017/07/24/how-to-choose-and-manage-extracurriculars-as-a-first-year/"
+        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/XPVAMKSJENGVDN7M5RBT5JOW6Y.jpg",
+        article_title: "How Harlem restaurants are staying afloat amid the pandemic",
+        article_authors: "Bella Druckman and Noah Sheidlower",
+        article_link: "https://www.columbiaspectator.com/arts-and-entertainment/2020/12/09/how-harlem-restaurants-are-staying-afloat-amid-the-pandemic/"
     },
     {
-        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/37EFT3YKOJE2ZCAARGOM6TQEGQ.png",
-        article_title: "How to choose and manage extracurriculars as a first-year",
-        article_authors: "Victoria Yang",
-        article_link: "https://www.columbiaspectator.com/required-reading/2017/07/24/how-to-choose-and-manage-extracurriculars-as-a-first-year/"
+        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/XHIIDEE7SNF5BFRE64N77VWW3I.jpg",
+        article_title: "Wallach Art Gallery’s Uptown Triennial 2020 showcases artists of color in dialogue with the Harlem Renaissance",
+        article_authors: "Vincent Hou and Camilla Marchese Gonzalez",
+        article_link: "https://www.columbiaspectator.com/arts-and-entertainment/2020/11/24/wallach-art-gallerys-uptown-triennial-2020-showcases-artists-of-color-in-dialogue-with-the-harlem-renaissance/"
     },
     {
-        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/37EFT3YKOJE2ZCAARGOM6TQEGQ.png",
-        article_title: "How to choose and manage extracurriculars as a first-year",
-        article_authors: "Victoria Yang",
-        article_link: "https://www.columbiaspectator.com/required-reading/2017/07/24/how-to-choose-and-manage-extracurriculars-as-a-first-year/"
+        image_url: "https://spectator-static-assets.s3.amazonaws.com/covid-vaccine/thumbnail-Artboard_1.96b71597.svg",
+        article_title: "A COVID-19 vaccine is on its way, but a majority of West Harlem residents won’t see it anytime soonr",
+        article_authors: "Elizabeth Commisso and Chloe Kok",
+        article_link: "https://www.columbiaspectator.com/news/2020/08/04/a-covid-19-vaccine-is-on-its-way-but-a-majority-of-west-harlem-residents-wont-see-it-anytime-soon/"
     },
     {
-        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/37EFT3YKOJE2ZCAARGOM6TQEGQ.png",
-        article_title: "How to choose and manage extracurriculars as a first-year",
-        article_authors: "Victoria Yang",
-        article_link: "https://www.columbiaspectator.com/required-reading/2017/07/24/how-to-choose-and-manage-extracurriculars-as-a-first-year/"
+        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/GLQQP4WSO5EF3CWJJNA7CMXSLY.JPG",
+        article_title: "Local businesses eagerly anticipate students’ return to campus while grappling with surge in COVID-19 cases",
+        article_authors: "Octavio Galaviz",
+        article_link: "https://www.columbiaspectator.com/news/2020/11/23/local-businesses-eagerly-anticipate-students-return-to-campus-while-grappling-with-surge-in-covid-19-cases/"
+    },
+    {
+        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/J7USDTWSWFBUJGEV6G6OEL3N2Q.JPG",
+        article_title: "Columbia canceled housing contracts, so 14,000 students moved into the city. What does this mean for the local housing market?",
+        article_authors: "Abby Melbourne and Stephanie Lai",
+        article_link: "https://www.columbiaspectator.com/news/2020/11/11/columbia-canceled-housing-contracts-so-14000-students-moved-into-the-city-what-does-this-mean-for-the-local-housing-market/"
     }
 ];
