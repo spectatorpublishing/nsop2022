2792652e6376bbe2f31f07f929a9433fbe4c7b71
sarahbraka <69284826+sarahbraka@users.noreply.github.com>
2021-01-19 01:20:22 -0500

    damn arc be crazy but i started this

--- src/data/on_campus_articles.js
+++ src/data/on_campus_articles.js
@@ -1,15 +1,15 @@
 export const on_campus_articles = [
     {
-        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/37EFT3YKOJE2ZCAARGOM6TQEGQ.png",
-        article_title: "How to choose and manage extracurriculars as a first-year",
-        article_authors: "Victoria Yang",
-        article_link: "https://www.columbiaspectator.com/required-reading/2017/07/24/how-to-choose-and-manage-extracurriculars-as-a-first-year/"
+        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/5NAL2ERLFFFVTLN2JGNBJFIBNI.gif",
+        article_title: "The New Columbia Bubble: First Generation, Low Income Students Form The Majority In Pandemic-Era Dorms",
+        article_authors: "Briani Netzahuatl",
+        article_link: "https://www.columbiaspectator.com/the-eye/2020/12/14/new-columbia-bubble-first-generation-low-income-students-form-the-majority-in-pandemic-era-dorms/"
     },
     {
-        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/37EFT3YKOJE2ZCAARGOM6TQEGQ.png",
-        article_title: "How to choose and manage extracurriculars as a first-year",
-        article_authors: "Victoria Yang",
-        article_link: "https://www.columbiaspectator.com/required-reading/2017/07/24/how-to-choose-and-manage-extracurriculars-as-a-first-year/"
+        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VFTQ2IBHOBBITKXFMV6FLECOIA.png",
+        article_title: "The Many Lives of Greek Life",
+        article_authors: "Teresa Lawlor",
+        article_link: "https://www.columbiaspectator.com/the-eye/2020/12/04/the-many-lives-of-greek-life/"
     },
     {
         image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/37EFT3YKOJE2ZCAARGOM6TQEGQ.png",
