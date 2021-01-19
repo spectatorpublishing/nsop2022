dc63d4c7ca0ce596793a6cf1a01bdb2c04473290
sarahbraka <69284826+sarahbraka@users.noreply.github.com>
2021-01-18 23:56:29 -0500

    Update resource_articles.js

--- src/data/resource_articles.js
+++ src/data/resource_articles.js
@@ -1,38 +1,56 @@
 export const resource_articles = [
     {
-        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/37EFT3YKOJE2ZCAARGOM6TQEGQ.png",
-        article_title: "How to choose and manage extracurriculars as a first-year",
-        article_authors: "Victoria Yang",
-        article_link: "https://www.columbiaspectator.com/required-reading/2017/07/24/how-to-choose-and-manage-extracurriculars-as-a-first-year/"
+        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/EZJJ32WVA5G6ZHVL3V7YZ4DHZU.jpg",
+        article_title: "A master list for your off-campus apartment",
+        article_authors: "Ariana Novo",
+        article_link: "https://www.columbiaspectator.com/spectrum/2020/11/08/a-master-list-for-your-off-campus-apartment/"
     },
     {
-        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/37EFT3YKOJE2ZCAARGOM6TQEGQ.png",
-        article_title: "How to choose and manage extracurriculars as a first-year",
-        article_authors: "Victoria Yang",
-        article_link: "https://www.columbiaspectator.com/required-reading/2017/07/24/how-to-choose-and-manage-extracurriculars-as-a-first-year/"
+        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/YOJTHLFFEZGDTL75HFVDWPUX34",
+        article_title: "Global Center and WeWork study spaces, explained",
+        article_authors: "Swethaa Suresh",
+        article_link: "https://www.columbiaspectator.com/spectrum/2020/10/18/global-center-and-wework-study-spaces-explained/"
     },
     {
-        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/37EFT3YKOJE2ZCAARGOM6TQEGQ.png",
-        article_title: "How to choose and manage extracurriculars as a first-year",
-        article_authors: "Victoria Yang",
-        article_link: "https://www.columbiaspectator.com/required-reading/2017/07/24/how-to-choose-and-manage-extracurriculars-as-a-first-year/"
+        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/BOQEKKS5JNHRRGXLLQE7RK3LRM.gif",
+        article_title: "Questions to ask yourself before applying for internships as a first-year",
+        article_authors: "Jane Mok",
+        article_link: "https://www.columbiaspectator.com/spectrum/2020/08/12/questions-to-ask-yourself-before-applying-for-internships-as-a-first-year/"
     },
     {
-        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/37EFT3YKOJE2ZCAARGOM6TQEGQ.png",
-        article_title: "How to choose and manage extracurriculars as a first-year",
-        article_authors: "Victoria Yang",
-        article_link: "https://www.columbiaspectator.com/required-reading/2017/07/24/how-to-choose-and-manage-extracurriculars-as-a-first-year/"
+        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/XHW5H5ZK6NA4NB3RSWEH443YYQ.jpg",
+        article_title: "How to use LinkedIn and Handshake effectively",
+        article_authors: "Jazilah Salam",
+        article_link: "https://www.columbiaspectator.com/spectrum/2020/03/10/how-to-use-linkedin-and-handshake-effectively/"
     },
     {
-        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/37EFT3YKOJE2ZCAARGOM6TQEGQ.png",
-        article_title: "How to choose and manage extracurriculars as a first-year",
-        article_authors: "Victoria Yang",
-        article_link: "https://www.columbiaspectator.com/required-reading/2017/07/24/how-to-choose-and-manage-extracurriculars-as-a-first-year/"
+        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/CPU5YUOCK5FGDPPAVKZMKSUR44",
+        article_title: "Lost in translation: A comprehensive guide to communicating with your professor",
+        article_authors: "Nandini Talwar",
+        article_link: "https://www.columbiaspectator.com/spectrum/2020/11/15/lost-in-translation-a-comprehensive-guide-to-communicating-with-your-professor/"
     },
     {
-        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/37EFT3YKOJE2ZCAARGOM6TQEGQ.png",
-        article_title: "How to choose and manage extracurriculars as a first-year",
-        article_authors: "Victoria Yang",
-        article_link: "https://www.columbiaspectator.com/required-reading/2017/07/24/how-to-choose-and-manage-extracurriculars-as-a-first-year/"
+        image_url: "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/DJWKX53OQJFK5NQ2VXMQQFOIFU.jpg",
+        article_title: "A guide to securing a campus affiliated job",
+        article_authors: "Jane Mok",
+        article_link: "https://www.columbiaspectator.com/spectrum/2020/03/04/a-guide-to-securing-a-campus-affiliated-job/"
+    },
+    {
+        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/LBQMFAS3TZD6HORHFC3AWTZN2E.jpg",
+        article_title: "What is Beyond Barnard, and how can it help you?",
+        article_authors: "Jazilah Salam",
+        article_link: "https://www.columbiaspectator.com/spectrum/2019/10/02/what-is-beyond-barnard-and-how-can-it-help-you/"
+    },
+    {
+        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/OLBHHFFZHZHWTKAIG6LKMB632Y.jpg",
+        article_title: "You were rejected from a club. Now what?",
+        article_authors: "Nandini Talwar",
+        article_link: "https://www.columbiaspectator.com/spectrum/2020/10/15/you-were-rejected-from-a-club-now-what/"
+    },
+    {
+        image_url: "https://www.columbiaspectator.com/spectrum/2020/11/16/an-easy-guide-to-academic-help-resources/",
+        article_title: "An easy guide to academic help resources",
+        article_authors: "Charlotte Wu",
+        article_link: "https://www.columbiaspectator.com/spectrum/2020/11/16/an-easy-guide-to-academic-help-resources/"
     }
 ];
