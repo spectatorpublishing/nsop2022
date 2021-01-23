210d69a3cf82b4a69145f705ccb1b72c7f85f432
Erin <erin2722@gmail.com>
2021-01-23 14:38:09 -0500

    :sparkles:

--- src/containers/CreditsPage.js
+++ src/containers/CreditsPage.js
@@ -105,7 +105,6 @@ const CreditsPage = () => {
                             <p>Octavio Galaviz</p>
                             <p>Kathy Fang</p>
                         </div>
-
                         <div>
                             <h3>Arts and Entertainment</h3>
                             <p>Noah Sheidlower, A&E Editor</p>
@@ -115,33 +114,6 @@ const CreditsPage = () => {
                             <p>Vincent Hou</p>
                             <p>Esterah Brown</p>
                         </div>
-
-                        <div>
-                        <h3>Graphics</h3>
-                        <p>Jessica Li, Graphics Editor</p>
-                        <p>Charlotte Li, Graphics Deputy Editor</p>
-                        <p>Liz Commisso</p>
-                        <p>Raeedah Wahid</p>
-                        </div>
-
-                        <div>
-                            <h3>Photo</h3>
-                            <p>Beatrice Shlansky, Photo Editor</p>
-                            <p>Millie Felder, Deputy Photo Editor</p>
-                        </div>
-                    </SubDiv>
-                    <SubDiv>
-                        <div>
-                            <h3>The Eye</h3>
-                            <p>Claudia Gohn, Managing Editor of The Eye</p>
-                            <p>Cole Cahill, Lead Story Editor</p>
-                            <p>Teresa Lawlor, View from Here Editor</p>
-                            <p>Briani Netzahuatl, Features Editor</p>
-                            <p>Derek Ng</p>
-                            <p>Eve Washington</p>
-                            <p>Cassie Wu</p>
-                        </div>
-
                         <div>
                             <h3>Illustrations</h3>
                             <p>Brenda Huang, Illustrations Editor</p>
@@ -153,7 +125,33 @@ const CreditsPage = () => {
                             <p>Sadia Sharif</p>
                             <p>Nicole Yang</p>
                         </div>
-
+                        <div>
+                            <h3>Photo</h3>
+                            <p>Beatrice Shlansky, Photo Editor</p>
+                            <p>Millie Felder, Deputy Photo Editor</p>
+                            <p>Kate Della Pietra, Deputy Photo Editor</p>
+                            <p>Elisabeth McLaughlin, Deputy Photo Editor</p>
+                            <p>Samantha Camacho</p>
+                            <p>Rommel Nunez</p>
+                        </div>
+                    </SubDiv>
+                    <SubDiv>
+                        <div>
+                            <h3>Opinion</h3>
+                            <p>Ryan Oden, Editorial Page Editor</p>
+                            <p>Gurtej Gill, Deputy Editorial Page Editor</p>
+                            <p>Senem Yurdakul, Deputy Editorial Page Editor</p>
+                            <p>Lily Nathanson, Deputy Editorial Page Editor</p>
+                            <p>Karlton Gaskin, Deputy Editorial Page Editor</p>
+                            <p>Hsinyen Huang, Deputy Editorial Page Editor</p>
+                            <p>Casey Qi</p>
+                            <p>Celeste Ramirez</p>
+                            <p>Sara Chough</p>
+                            <p>Isabella Inouye</p>
+                            <p>Danya Jafri</p>
+                            <p>John Towfighi</p>
+                            <p>Tamarah Wallace</p>
+                        </div>
                         <div>
                             <h3>Sports</h3>
                             <p>Mackenzie George, Sports Editor</p>
@@ -165,11 +163,22 @@ const CreditsPage = () => {
                             <p>Juliet Tochterman</p>
                         </div>
                         <div>
-                            <h3>Video</h3>
-                            <p>Ryan Balderas</p>
-                            <p>Caitlin Buckley</p>
-                            <p>Rommel Nunez</p>
-                        </div>     
+                            <h3>Graphics</h3>
+                            <p>Jessica Li, Graphics Editor</p>
+                            <p>Charlotte Li, Graphics Deputy Editor</p>
+                            <p>Liz Commisso</p>
+                            <p>Raeedah Wahid</p>
+                        </div> 
+                        <div>
+                            <h3>Spectrum</h3>
+                            <p>Ariana Novo, Deputy Spectrum Editor</p>
+                            <p>Lina Bennani Karim, Deputy Spectrum Editor</p>
+                            <p>Jane Mok, Deputy Spectrum Editor</p>
+                            <p>Swethaa Suresh</p>
+                            <p>Jazilah Salam</p>
+                            <p>Nandini Talwar</p>
+                            <p>Charlotte Wu</p>
+                        </div>   
                     </SubDiv>
                     <SubDiv>
                         <div>
@@ -199,15 +208,20 @@ const CreditsPage = () => {
                             <p>Renuka Balakrishnan</p>
                         </div>
                         <div>
-                            <h3>Spectrum</h3>
-                            <p>Ariana Novo, Deputy Spectrum Editor</p>
-                            <p>Lina Bennani Karim, Deputy Spectrum Editor</p>
-                            <p>Jane Mok, Deputy Spectrum Editor</p>
-                            <p>Swethaa Suresh</p>
-                            <p>Jazilah Salam</p>
-                            <p>Nandini Talwar</p>
-                            <p>Charlotte Wu</p>
+                            <h3>The Eye</h3>
+                            <p>Claudia Gohn, Managing Editor of The Eye</p>
+                            <p>Cole Cahill, Lead Story Editor</p>
+                            <p>Teresa Lawlor, View from Here Editor</p>
+                            <p>Briani Netzahuatl, Features Editor</p>
+                            <p>Derek Ng</p>
+                            <p>Eve Washington</p>
+                            <p>Cassie Wu</p>
                         </div>
+                        <div>
+                            <h3>Video</h3>
+                            <p>Ryan Balderas</p>
+                            <p>Caitlin Buckley</p>
+                        </div> 
                     </SubDiv>
                 </Row>
             </Text>
