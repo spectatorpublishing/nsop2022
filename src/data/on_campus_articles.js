7129d0963c0f295f2239c641806ba1d96fef0a71
sarahbraka <69284826+sarahbraka@users.noreply.github.com>
2021-01-22 21:22:23 -0500

    Update on_campus_articles.js

--- src/data/on_campus_articles.js
+++ src/data/on_campus_articles.js
@@ -12,10 +12,10 @@ export const on_campus_articles = [
         article_link: "https://www.columbiaspectator.com/the-eye/2020/12/04/the-many-lives-of-greek-life/"
     },
     {
-        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/37EFT3YKOJE2ZCAARGOM6TQEGQ.png",
-        article_title: "How to choose and manage extracurriculars as a first-year",
-        article_authors: "Victoria Yang",
-        article_link: "https://www.columbiaspectator.com/required-reading/2017/07/24/how-to-choose-and-manage-extracurriculars-as-a-first-year/"
+        image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/2GDOYBTXOVFBHO4TOM3Y2WYRXY.jpg",
+        article_title: "Columbia invites 1,800 students back to campus despite social distancing violations and rising cases",
+        article_authors: "Talia Abrahamson",
+        article_link: "https://www.columbiaspectator.com/news/2021/01/21/columbia-invites-1800-students-back-to-campus-despite-social-distancing-violations-and-rising-cases/"
     },
     {
         image_url: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/37EFT3YKOJE2ZCAARGOM6TQEGQ.png",
