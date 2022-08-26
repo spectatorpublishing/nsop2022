39f89f4561aec4ba358a11fa863990a50b65eb5f
Marian Abuhazi <76821632+marianabuhazi@users.noreply.github.com>
2022-08-26 16:03:01 -0400

    Updated App.js
    Final link redirects back to home

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
         <Route exact path='/city-news' render={() => <Section id="city-news" header="CITY NEWS" articles={news_articles} next="Opinion" link="/opinion" key={1}/>} />
         <Route exact path='/opinion' render={() => <Section id="opinion" header="OPINION" articles={opinion_articles} next="Sports" link="/sports" key={2} />} />
         <Route exact path='/sports' render={() => <Section id="sports" header="SPORTS" articles={sports_articles} next="A&E" link="/a&e" key={3}/>} />
-        <Route exact path='/a&e' render={() => <Section id="a&e" header="A&E" articles={ae_articles} next="The Eye" link="/the-eye" key={4}/>} />
+        <Route exact path='/a&e' render={() => <Section id="a&e" header="A&E" articles={ae_articles} next="Home" link="/" key={4}/>} />
       </Switch>
     </Router>
   );
